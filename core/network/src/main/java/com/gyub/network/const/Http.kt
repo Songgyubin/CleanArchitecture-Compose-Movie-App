bec1e8148287b84d68dbb3a36b48bb3480f3b643
songgyubin <thd0427__@naver.com>
2024-02-29 23:57:19 +0900

    📦️ Network 의존성 주입 및 Http 상수 정의

---
+++ core/network/src/main/java/com/gyub/network/const/Http.kt
@@ -0,0 +1,51 @@
+package com.gyub.network.const
+
+/**
+ * 헤더, URL, Server Status 등
+ * Http 통신에 필요한 상수 정의
+ *
+ * @author   Gyub
+ * @created  2024/02/29
+ */
+object Http {
+
+    /**
+     * Http Header
+     */
+    object Headers {
+        const val AUTHORIZATION = "Authorization"
+        const val ACCEPT = "Accept"
+    }
+
+    /**
+     * Http Method
+     */
+    object Methods {
+        const val POST = "POST"
+        const val PUT = "PUT"
+    }
+
+    /**
+     * Api Url
+     */
+    object Url {
+        val BASE_URL: String by lazy { "https://api.themoviedb.org" }
+    }
+
+    /**
+     * Server Status
+     */
+    object Status {
+        const val SUCCESS = 200
+        const val SERVER_MAINTENANCE = 999
+        const val BAD_REQUEST = 400
+        const val TOKEN_ERROR = 401
+        const val VALIDATE_ERROR = 403
+        const val NOT_FOUND_ERROR = 404
+        const val UPGRADE_ERROR = 426
+        const val DEACTIVATE_ERROR = 308
+        const val DUPLICATE_ERROR = 412
+        const val NOT_FOUND = 404
+        const val SMS_CERTIFICATION_FAIL = 701
+    }
+}
---
+++ core/network/src/main/java/com/gyub/network/di/NetworkModule.kt
@@ -0,0 +1,40 @@
+package com.gyub.network.di
+
+import com.google.gson.Gson
+import com.gyub.network.const.Http.Url.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ *  Network Module
+ *
+ * @author   Gyub
+ * @created  2024/02/29
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideGson() = Gson()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gson: Gson,
+    ): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+    }
+
+}
