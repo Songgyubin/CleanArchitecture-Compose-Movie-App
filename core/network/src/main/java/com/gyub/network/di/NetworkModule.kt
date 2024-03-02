6f8ae2aece4c7299cb55710791a478b03bff4449
gyu_b <thd0427__@naver.com>
2024-03-02 17:03:26 +0900

    헤더 및 로거 세팅 (#3)

--- core/network/src/main/java/com/gyub/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/gyub/network/di/NetworkModule.kt
@@ -1,15 +1,22 @@
 package com.gyub.network.di
 
+import android.util.Log
 import com.google.gson.Gson
+import com.gyub.network.BuildConfig
 import com.gyub.network.const.Http.Url.BASE_URL
+import com.gyub.network.util.NetworkUtil
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
+import javax.net.ssl.SSLSession
 
 /**
  *  Network Module
@@ -19,10 +26,10 @@ import javax.inject.Singleton
  */
 @Module
 @InstallIn(SingletonComponent::class)
-internal object NetworkModule {
+object NetworkModule {
 
-    @Singleton
     @Provides
+    @Singleton
     fun provideGson() = Gson()
 
     @Singleton
@@ -35,5 +42,34 @@ internal object NetworkModule {
             .baseUrl(BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        @Named("RetrofitInterceptor") interceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(NetworkUtil.createHeader())
+            .addInterceptor(interceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    @Named("RetrofitInterceptor")
+    fun provideRetrofitInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor { message ->
+            Log.d("### Retrofit --", NetworkUtil.getPrettyLogs(message))
+        }.apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
     }
 }
---
+++ core/network/src/main/java/com/gyub/network/util/NetworkUtil.kt
@@ -0,0 +1,62 @@
+package com.gyub.network.util
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import com.gyub.network.BuildConfig.API_KEY
+import com.gyub.network.const.Http
+import okhttp3.Interceptor
+
+/**
+ *
+ *
+ * @author   Gyub
+ * @created  2024/03/02
+ */
+object NetworkUtil {
+    /**
+     * Pretty Log
+     *
+     * @param text json string
+     * @return 정렬된 Json Data
+     */
+    fun getPrettyLogs(text: String?): String {
+        text ?: return ""
+
+        return try {
+            val jsonParser = JsonParser()
+            val jsonElement = jsonParser.parse(text)
+
+            val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+            gson.toJson(jsonElement)
+        } catch (e: Throwable) {
+            text
+        }
+    }
+
+    /**
+     * 헤더 세팅
+     *
+     * @return
+     */
+    fun createHeader() =
+        Interceptor { chain: Interceptor.Chain ->
+            val original = chain.request()
+            val builder = original.newBuilder().apply {
+                getHeaders().forEach { (key, value) -> header(key, value) }
+            }
+            chain.proceed(builder.build())
+        }
+
+    /**
+     * 내부 저장소에 저장되어 있는 정보 Header 취합
+     *
+     * @return
+     */
+    private fun getHeaders(): Map<String, String> {
+        return mapOf(
+            Http.Headers.AUTHORIZATION to API_KEY,
+            Http.Headers.ACCEPT to "application/json",
+        )
+    }
+}
