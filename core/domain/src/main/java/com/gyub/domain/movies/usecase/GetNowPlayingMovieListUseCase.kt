c6f1b2fcefbbd847e4e52061f787269a58bfef67
songgyubin <thd0427__@naver.com>
2024-03-05 21:58:21 +0900

    📦️ 현재 상영중인 영화 목록 usecase 추가

--- core/domain/src/main/java/com/gyub/domain/movies/usecase/GetNowPlayingMovieListUseCase.kt
+++ core/domain/src/main/java/com/gyub/domain/movies/usecase/GetNowPlayingMovieListUseCase.kt
@@ -1,10 +1,22 @@
 package com.gyub.domain.movies.usecase
 
+import com.gyub.domain.movies.model.MovieListsEntity
+import com.gyub.domain.movies.repository.MovieListsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
 /**
  * 현재 극장에 상영 중인 영화 목록 UseCase
  *
  * @author   Gyub
  * @created  2024/03/02
  */
-class GetNowPlayingMovieListUseCase {
+class GetNowPlayingMovieListUseCase
+@Inject
+constructor(private val movieListsRepository: MovieListsRepository) {
+    operator fun invoke(): Flow<MovieListsEntity> = flow {
+        val item = movieListsRepository.getNowPlayingMovieList()
+        emit(item)
+    }
 }
