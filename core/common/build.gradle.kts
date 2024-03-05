8945eb96d576bc601f218a8cf554d553646abbc7
songgyubin <thd0427__@naver.com>
2024-03-05 22:29:01 +0900

    ➕ kotlin jvm 타겟 버전 세팅

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,9 +1,17 @@
+import definition.Configs
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
