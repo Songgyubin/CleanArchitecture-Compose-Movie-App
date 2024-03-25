ccd5d4ed5ce3b741afb98cacfabbdc6776831a72
songgyubin <thd0427__@naver.com>
2024-03-25 15:26:04 +0900

    📦️ Color 추가

---
+++ core/design/src/main/java/com/gyub/core/design/theme/Color.kt
@@ -0,0 +1,36 @@
+package com.gyub.core.design.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * 색상
+ *
+ * @author   Gyub
+ * @created  2024/03/25
+ */
+
+/**
+ * 주요 강조색상
+ */
+internal val GDSRed10 = Color(0xFF410002) // 매우 어두운 레드 색조
+internal val GDSRed20 = Color(0xFF690005) // 어두운 레드 색조
+internal val GDSRed30 = Color(0xFF93000A) // 더 밝은 어두운 레드 색조
+internal val GDSRed40 = Color(0xFFBA1A1A) // 중간 정도의 레드 색조
+internal val GDSRed80 = Color(0xFFFFB4AB) // 밝은 레드 색조
+internal val GDSRed90 = Color(0xFFFFDAD6) // 매우 밝은 레드 색조
+
+/**
+ * 배경색
+ */
+internal val GDSBlack = Color(0xFF000000)
+
+/**
+ * 텍스트 및 아이콘 색상
+ */
+internal val GDSWhite = Color(0xFFFFFFFF)
+
+
+/**
+ * 보조 텍스트 및 요소 색상
+ */
+internal val GDSGray = Color(0xFF808080)
