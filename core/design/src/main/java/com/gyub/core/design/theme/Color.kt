a9800c34e229cb1c17e1310580c83fdd9d348ce1
songgyubin <thd0427__@naver.com>
2024-03-26 15:33:33 +0900

    📦️ Color 및 Theme 추가

--- core/design/src/main/java/com/gyub/core/design/theme/Color.kt
+++ core/design/src/main/java/com/gyub/core/design/theme/Color.kt
@@ -18,17 +18,18 @@ internal val GDSRed30 = Color(0xFF93000A) // 더 밝은 어두운 레드 색조
 internal val GDSRed40 = Color(0xFFBA1A1A) // 중간 정도의 레드 색조
 internal val GDSRed80 = Color(0xFFFFB4AB) // 밝은 레드 색조
 internal val GDSRed90 = Color(0xFFFFDAD6) // 매우 밝은 레드 색조
+internal val DarkPurpleGray20 = Color(0xFF362F30)
 
-/**
- * 배경색
- */
+internal val Blue10 = Color(0xFF001F28)
+internal val Blue20 = Color(0xFF003544)
+internal val Blue30 = Color(0xFF004D61)
+internal val Blue40 = Color(0xFF006780)
+internal val Blue80 = Color(0xFF5DD5FC)
+internal val Blue90 = Color(0xFFB8EAFF)
+internal val PurpleGray50 = Color(0xFF7F747C)
+
 internal val GDSBlack = Color(0xFF000000)
-
-/**
- * 텍스트 및 아이콘 색상
- */
 internal val GDSWhite = Color(0xFFFFFFFF)
-
 
 /**
  * 보조 텍스트 및 요소 색상
